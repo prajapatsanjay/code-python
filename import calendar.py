10ad860199ecf29ce1c8636afc1e8f76943cb7f5
sanjay786 <prajapatsanjay942@gmail.com>
2025-08-26 13:24:05 +0530

    it's a order

---
+++ import calendar.py
@@ -0,0 +1,13 @@
+# # import calendar
+# # yy = 2025
+# # mm = 1
+
+# # print(calendar.month(yy,mm))
+
+
+import calendar
+print("THE calendar of year 2025 is : ")
+print(calendar.calendar(2025))
+
+
+
